09ce52aa9b90b66bcc1619d1761cde009089ca00
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 18:59:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +56,10 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -80,7 +74,13 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
     }
+    
+    programmer_hash
 
 end
 
