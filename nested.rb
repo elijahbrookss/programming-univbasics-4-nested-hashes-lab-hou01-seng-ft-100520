e1178b52cd3ca6dd2d412bcdde60e93687c2e685
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 19:01:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +127,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie]
 
 end
